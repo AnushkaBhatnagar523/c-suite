211550434162013a27eec08790b6623f212026be
Anushka <anushka@gmail.com>
2026-02-16 20:13:00 +0530

    Fix: Stop convertBigInt from destroying Date objects

--- backend/database/db.js
+++ backend/database/db.js
@@ -121,6 +121,9 @@ function convertBigInt(obj) {
     if (typeof obj === 'bigint') return Number(obj);
     if (Array.isArray(obj)) return obj.map(convertBigInt);
     if (typeof obj === 'object') {
+        // Don't recurse into Date objects
+        if (obj instanceof Date) return obj;
+
         const newObj = {};
         for (const key in obj) {
             newObj[key] = convertBigInt(obj[key]);
